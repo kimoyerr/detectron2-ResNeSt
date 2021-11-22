e705be35718e27d7b83ad16969cb6b6c795cd307
EC2 Default User <ec2-user@ip-172-31-46-247.us-west-2.compute.internal>
2021-11-22 10:44:38 +0000

    Add debug logging

--- detectron2/evaluation/evaluator.py
+++ detectron2/evaluation/evaluator.py
@@ -133,6 +133,9 @@ def inference_on_dataset(model, data_loader, evaluator):
     total_compute_time = 0
     with inference_context(model), torch.no_grad():
         for idx, inputs in enumerate(data_loader):
+            logger.debug(idx)
+            logger.debug(inputs)
+            logger.debug(inputs[0]['image'].shape)
             if idx == num_warmup:
                 start_time = time.perf_counter()
                 total_compute_time = 0
